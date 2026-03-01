e96cd17bbd3af5dfd52ca926cfe6d1caeb7d06b4
clark45767 <clarkigarma@gmail.com>
2026-03-01 11:09:28 +0400

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,11 @@
+my_set ={1,3,67,34,56,5,6,7,8,95,3,5,67,21,21}
+print(my_set)
+my_set.add(100)
+print(my_set)
+my_set2 = {67,89,31,3,5,21}
+print(my_set.difference(my_set2))
+print(my_set.symmetric_difference(my_set2))
+set1 = { "orange", "green", "white"}
+set2 = {"Black","White","Yellow"}
+print(set1.union(set2))
+print(set1.intersection(set2))
